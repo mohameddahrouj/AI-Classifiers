f83fbf4765a321abf5ef2a0b1ec69a4354f15878
Mohamed Dahrouj <dahrouj.m@gmail.com>
2017-05-13 11:11:03 -0400

    Created new app and configured VS settings

---
+++ Classifiers/AI-Classifiers/ClassifiersUI.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using Classifiers.ViewModel;
+using System.IO;
+
+namespace AI_Classifiers
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            string _filePath = Path.GetDirectoryName(System.AppDomain.CurrentDomain.BaseDirectory);
+            _filePath = Directory.GetParent(_filePath).FullName;
+            _filePath = Directory.GetParent(Directory.GetParent(_filePath).FullName).FullName;
+            _filePath += @"\AI-Classifiers\Datasets\ArtificialData.csv";
+            this.DataContext = new ClassifierViewModel(_filePath);
+            InitializeComponent();
+        }
+
+        private void IndependentClassification(object sender, RoutedEventArgs e)
+        {
+            ((ClassifierViewModel)this.DataContext).IndependentClassification();
+        }
+
+        private void DependentClassification(object sender, RoutedEventArgs e)
+        {
+            ((ClassifierViewModel)this.DataContext).DependentClassifcation();
+        }
+
+        private void DecisionClassification(object sender, RoutedEventArgs e)
+        {
+            ((ClassifierViewModel)this.DataContext).DecisionClassification();
+        }
+    }
+}
